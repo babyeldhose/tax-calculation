e7185afe7a92c6d838c8b0e7a0e5ef06e99144a5
c0654335 <c0654335@SB-N208-29.lambton.on.ca>
2015-01-13 09:28:12 -0500

    first commit

---
+++ JavaApplication2.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication2;
+
+/**
+ *
+ * @author c0654335
+ */
+public class JavaApplication2 {
+
+   
+    
+  
+    public static void main(String[] args) {
+        double x= 132.85;
+    double y=(x*13)/100;
+    double z=x+y;
+    
+        System.out.println("the actual price is="+x);
+                System.out.println("the tax is="+y);
+                        System.out.println("the total price is="+z);
+
+
+        
+    }
+    
+}
